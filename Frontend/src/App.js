47ad3b023b4bec73f5e58dfb0bcd8148b8bdbf31
MayaraSs <may_stein@hotmail.com>
2020-05-24 14:53:30 -0300

    concepts of state and immutability

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+  const [projects, setProjects] = useState(["react", "Javascript"]);
+
+  function handleAddProject() {
+    //projects.push(`Novo projeto ${Date.now()}`);
+
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="Login">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
       <Header title="Inicio" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 }
--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { ProgressPlugin } from "webpack";
 
-export default function Header({ title, children }) {
+export default function Header({ title }) {
   return (
     <header>
       <h1>{title}</h1>
-      {children}
     </header>
   );
 }
